d35c3be6f7e859e3b106f1825dca825739cf235c
sophiezhaoo <167384601+sophiezhaoo@users.noreply.github.com>
2024-05-29 21:06:06 -0400

    Add files via upload

---
+++ success.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
+
+setTimeout(() => {
+    window.location.href = window.location.origin
+  }, 5000)
